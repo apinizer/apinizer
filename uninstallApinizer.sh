9fb47b3475f56e8d039281850597471909c804c1
apinizerdev <84512911+apinizerdev@users.noreply.github.com>
2021-07-18 14:44:22 +0300

    Update uninstallApinizer.sh

--- uninstallApinizer.sh
+++ uninstallApinizer.sh
@@ -7,9 +7,9 @@ sudo service mongod stop
 
 sudo apt-get purge mongodb-org* -y
 
-sudo rm -r /var/log/mongodb
+sudo rm -rf /var/log/mongodb
 
-sudo rm -r /var/lib/mongodb
+sudo rm -rf /var/lib/mongodb
 
 ### Uninstall Kubernetes
 
@@ -27,7 +27,7 @@ sudo apt-get purge -y docker-engine docker docker.io docker-ce docker-ce-cli
 sudo apt-get autoremove -y --purge docker-engine docker docker.io docker-ce  
 
 sudo rm -rf /var/lib/docker /etc/docker
-sudo rm /etc/apparmor.d/docker
+sudo rm -rf /etc/apparmor.d/docker
 sudo groupdel docker
 sudo rm -rf /var/run/docker.sock
 
