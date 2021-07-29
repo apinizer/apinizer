b84708d74f0873b2f232bc0620e8622f166223d9
apinizerdev <84512911+apinizerdev@users.noreply.github.com>
2021-07-29 15:39:27 +0300

    Update changeElasticIp.js

--- changeElasticIp.js
+++ changeElasticIp.js
@@ -1,4 +1,4 @@
 db.environment_log_server.updateOne(
 { "name": "ElasticsearchLocal" },
-{ "$set": { "elasticHostList.$[].host": "10.0.1.1" } }
+{ "$set": { "elasticHostList.$[].host": nodeIpPort } }
 )
