a9f1cac4de4d0040fec55190b2a087a0711f0515
apinizerdev <84512911+apinizerdev@users.noreply.github.com>
2021-08-31 10:31:30 +0300

    Update changeElasticIp.js

--- changeElasticIp.js
+++ changeElasticIp.js
@@ -1,7 +1,8 @@
 db.environment_settings.updateOne(
 { "name": "prod-env" },
-{ "$set": { "accessUrl": nodeIpPort } }
+{ "$set": { "accessUrl": "http://"+nodeIpPort+":30080" } }
 );
+
 
 db.environment_log_server.updateOne(
 { "name": "ElasticsearchLocal" },
