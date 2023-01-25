4615101513cecb8a4112fcb22a8e98c16fd85a99
apinizerdev <84512911+apinizerdev@users.noreply.github.com>
2023-01-25 17:23:56 +0300

    Update installApinizer.sh

--- installApinizer.sh
+++ installApinizer.sh
@@ -81,7 +81,7 @@ sudo apt update
 sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
 # Add Docker repo
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" --yes
 
 # Install containerd
 sudo apt update
