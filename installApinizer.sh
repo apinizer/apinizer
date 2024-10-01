2c7316031f65642e68461b92b793270699592ff7
apinizerdev <84512911+apinizerdev@users.noreply.github.com>
2024-10-01 22:30:24 +0300

    Update installApinizer.sh

--- installApinizer.sh
+++ installApinizer.sh
@@ -184,8 +184,8 @@ sudo apt update
 sudo apt install -y curl wget net-tools gnupg2 software-properties-common apt-transport-https ca-certificates lsb-release
  
  
-wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb
-sudo dpkg -i ./libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb
+wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.20_amd64.deb
+sudo dpkg -i ./libssl1.1_1.1.1f-1ubuntu2.20_amd64.deb
  
  
 curl -fsSL https://www.mongodb.org/static/pgp/server-6.0.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/mongodb-6.gpg
