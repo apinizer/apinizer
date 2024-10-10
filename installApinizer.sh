6011c46b00d9b9093191d5a8ed033b4c0de8d8af
apinizerdev <84512911+apinizerdev@users.noreply.github.com>
2024-10-10 16:25:38 +0300

    Update installApinizer.sh

--- installApinizer.sh
+++ installApinizer.sh
@@ -183,9 +183,9 @@ sudo apt update
 sudo apt install -y curl wget net-tools gnupg2 software-properties-common apt-transport-https ca-certificates lsb-release
  
  
-wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.20_amd64.deb
-sudo dpkg -i ./libssl1.1_1.1.1f-1ubuntu2.20_amd64.deb
- 
+wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
+sudo dpkg -i ./libssl1.1_1.1.1f-1ubuntu2_amd64.deb
+
  
 curl -fsSL https://www.mongodb.org/static/pgp/server-6.0.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/mongodb-6.gpg
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
